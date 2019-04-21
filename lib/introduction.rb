5a8e7e95d58de49a32e79ca95957496fe0d4130e
Cody Frank <cody.frank30@gmail.com>
2019-04-21 22:46:24 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -4,6 +4,10 @@ def introduction (name)
   puts "Hi, my name is #{name}."
 end
 
+<<<<<<< HEAD
 def introduction_with_language (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
+=======
+def 
+>>>>>>> 03158d8e1717e772754eea2260b1b9ac429dc57e
